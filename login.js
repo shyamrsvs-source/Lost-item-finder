29a6781b28ccc550581e7cf58f6b1b8a303315e7
dharunsubramanian07-Igtm <dharunsubramanian07@gmail.com>
2026-02-16 11:15:31 +0530

    5

--- login.js
+++ login.js
@@ -1,53 +1,47 @@
+const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+let captcha = "";
+for (let i = 0; i < 5; i++) {
+    captcha += chars.charAt(Math.floor(Math.random() * chars.length));
+}
+document.getElementById("captchaText").textContent = captcha;
 
-function returnValidationForm() {
+const form = document.getElementById("loginForm");
 
-    // Input values
+form.addEventListener("submit", function(event) {
+    event.preventDefault(); 
+
+  
     let name = document.getElementById("username").value.trim();
     let email = document.getElementById("email").value.trim();
     let pass = document.getElementById("password").value.trim();
 
-    // Correct credentials
-    let correctUser = "admin";
-    let correctEmail = "admin@lostfound.com";
-    let correctPass = "admin123";
+
+    document.getElementById("nameerror").textContent = "";
+    document.getElementById("emailerror").textContent = "";
+    document.getElementById("passerror").textContent = "";
 
     let valid = true;
 
-    // Clear errors
-    document.getElementById("nameerror").innerHTML = "";
-    document.getElementById("emailerror").innerHTML = "";
-    document.getElementById("passerror").innerHTML = "";
-
-    // Empty validations
     if (name === "") {
-        document.getElementById("nameerror").innerHTML = "Username required";
+        document.getElementById("nameerror").textContent = "Username required";
         valid = false;
     }
 
     if (email === "") {
-        document.getElementById("emailerror").innerHTML = "Email required";
+        document.getElementById("emailerror").textContent = "Email required";
         valid = false;
     }
 
     if (pass === "") {
-        document.getElementById("passerror").innerHTML = "Password required";
+        document.getElementById("passerror").textContent = "Password required";
         valid = false;
     } else if (pass.length < 6) {
-        document.getElementById("passerror").innerHTML = "Minimum 6 characters";
+        document.getElementById("passerror").textContent = "Minimum 6 characters";
         valid = false;
     }
 
-    // Credential check
+
     if (valid) {
-        if (name === correctUser && email === correctEmail && pass === correctPass) {
-            alert("Login Successful ✅");
-            return true; // allow submit
-        } else {
-            alert("Invalid login credentials ❌");
-            return false; // stop submit
-        }
+        document.getElementById("msg").textContent = "Login Successful ✅";
     }
-
-    return false;
-}
-
+});
